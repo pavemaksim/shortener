b5ca13e1ffe466ae15bd0c8d7f6ed5c327e181f6
pavemaksim <pavemaksim@gmail.com>
2020-05-31 19:25:06 +0300

    ShortCode algo has been changed

--- symfony/src/Service/ShortCode/ShortCodeGenerator.php
+++ symfony/src/Service/ShortCode/ShortCodeGenerator.php
@@ -1,24 +1,45 @@
 <?php
 
-
 namespace App\Service\ShortCode;
 
 /**
  * This is a simple example of generator for short codes
+ * Simply converts i with base 10 to some str with base 62
  *
  * Class ShortCodeGenerator
  * @package App\Service\Url
  */
 class ShortCodeGenerator implements ShortCodeInterface
 {
-    /**
-     * Generates a short code by using hex of the current timestamp
-     * WARNING: NOT race-condition-safe
-     *
-     * @return float|int
-     */
-    public function generateCode(int $id)
+    public $dictionary = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public function __construct()
     {
-        return dechex(time());
+        $this->dictionary = str_split($this->dictionary);
+    }
+
+    /**
+     * Generates a unique code based on provided integer $i
+     *
+     * @param int $i
+     * @return mixed|string
+     */
+    public function generateCode(int $i)
+    {
+        if ($i == 0) {
+            return $this->dictionary[0];
+        }
+
+        $result = [];
+        $base = count($this->dictionary);
+
+        while ($i > 0) {
+            $result[] = $this->dictionary[($i % $base)];
+            $i = floor($i / $base);
+        }
+
+        $result = array_reverse($result);
+
+        return join("", $result);
     }
 }
